ab17c8b9d5bd2cbab13f07fbb91c4d0042d88fdd
bpotter8 <144744976+bpotter8@users.noreply.github.com>
2024-02-06 13:48:56 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,20 @@
 function binarySearch(list, element) {
+    let leftIndex = 0;
+    let rightIndex = list.length - 1;
+
+    while(leftIndex <= rightIndex){
+        let midIndex = Math.floor((leftIndex + rightIndex) / 2);
+        
+        if(element == list[midIndex]){
+            return midIndex;
+        }
+        
+        if(element > list[midIndex]){
+            leftIndex = midIndex + 1;
+        } else {
+            rightIndex = midIndex - 1;
+        }
+    }
+        
     return -1;
 }
